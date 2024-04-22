73ca3a52bf8ace57df51e7c9d6547bf8ea4b9764
MuhammedELSayedBrq <mohamed999755@gmail.com>
2024-04-22 22:39:31 +0200

    get_ras_ip prod ready

--- get_ras_ip.py
+++ get_ras_ip.py
@@ -1,20 +1,10 @@
 import subprocess
 
 def get_connected_devices():
-    """
-    Uses `arp -a` to scan your network and return a list of connected devices,
-    handling potential index errors and providing informative error messages.
-
-    Returns:
-        list: A list of dictionaries containing details of connected devices:
-              [{'ip': '192.168.1.2', 'mac': '00:11:22:33:44:55', 'device_name': 'unknown'}, ...]
-    """
-
     try:
         output = subprocess.check_output(["arp", "-a"]).decode("utf-8")
         lines = output.splitlines()
 
-        # Skip header and empty lines
         devices = []
         for line in lines[2:]:
             if not line:
@@ -29,14 +19,13 @@ def get_connected_devices():
                 device_name = " ".join(parts[4:]) if len(parts) > 4 else "unknown"
 
             except IndexError:
-                #print(f"Warning: Unexpected ARP output format on line: {line}")
                 devices.append(line)
 
         return devices
 
     except subprocess.CalledProcessError as e:
         print(f"Error running `arp -a`: {e}")
-        return []  # Return an empty list on error
+        return []
 
 def get_ip():
     connected_devices = get_connected_devices()
